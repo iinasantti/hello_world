c89170ab45b47dba9506c78d9de26a9894046abb
iinasantti <iina.santti@tuni.fi>
2025-07-04 15:32:37 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,9 @@
 print("Hello world!")
-print("Hello once again")
-print("Hello world!")
-print("Hello darkness my old friend")
-
-print("I've come to talk with you again")
 print("Not another hello world")
+
+
+
+
 print("No")
 print("More")
 print("Hello")
